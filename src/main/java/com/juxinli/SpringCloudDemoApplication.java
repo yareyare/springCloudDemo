57d43f8f89213f375a53a4e5913a5accf524325b
zhangyan1 <zhangyan1@juxinli.com>
2018-07-15 13:07:49 +0800

    something

--- src/main/java/com/juxinli/SpringCloudDemoApplication.java
+++ src/main/java/com/juxinli/SpringCloudDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
-@EnableEurekaServer // eureka 启动一个服务注册中你提供给其他应用进行对话
+@EnableEurekaServer // eureka 启动一个服务注册中心提供给其他应用进行对话
 @SpringBootApplication
 public class SpringCloudDemoApplication {
 
